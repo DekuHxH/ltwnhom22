260ea8936e3507a4a2afaccc9266a8b2773edfce
Jv7680 <85338588+Jv7680@users.noreply.github.com>
2022-12-15 11:53:25 +0700

    Update RegisterController.java

--- BanNongSanLTW/src/main/java/controller/RegisterController.java
+++ BanNongSanLTW/src/main/java/controller/RegisterController.java
@@ -53,8 +53,10 @@ public class RegisterController extends HttpServlet {
 				
 				Thread.sleep(3000);
 				
-				RequestDispatcher rd = request.getRequestDispatcher("/");
-				rd.forward(request, response);
+				//RequestDispatcher rd = request.getRequestDispatcher("/");
+				//rd.forward(request, response);
+				
+				response.sendRedirect(request.getContextPath() + "/");
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
